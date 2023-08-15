54153be20c3d5af82c225dd0a6e59ba272b795e3
sergiosssg <sergiosssg@gmail.com>
2023-08-15 21:05:53 +0300

    picked up telegram token

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+var BotTokem = "6592601003:AAFBLd-o2bDUxhM_nMsWV74B3Rc_rl_-abc";
+
+
 Console.WriteLine("Hello, World!");
+
+
+
