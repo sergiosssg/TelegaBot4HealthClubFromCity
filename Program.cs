432803b02d8877122d97d230735a88f623ce649f
sergiosssg <sergiosssg@gmail.com>
2023-08-15 22:35:54 +0300

    consoleReadLine

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using Telegram.Bot;
+
 var BotTokem = "6592601003:AAFBLd-o2bDUxhM_nMsWV74B3Rc_rl_-abc";
 
 
+var BotClient = new TelegramBotClient(BotTokem);
+
 Console.WriteLine("Hello, World!");
+
+Console.ReadLine();
 
 
 
